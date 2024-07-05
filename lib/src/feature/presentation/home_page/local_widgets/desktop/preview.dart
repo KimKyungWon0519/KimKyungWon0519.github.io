6d83f6ce43ae13f2e6f2c641ec3ca2609bd22c60
KimKyungWon0519 <gwon080870@gmail.com>
2024-07-05 16:40:59 +0900

    FEAT: Create Preview Title

--- lib/src/feature/presentation/home_page/local_widgets/desktop/preview.dart
+++ lib/src/feature/presentation/home_page/local_widgets/desktop/preview.dart
@@ -9,10 +9,26 @@ class Preview extends StatelessWidget {
       margin: const EdgeInsets.all(8),
       child: Padding(
         padding: const EdgeInsets.all(16),
-        child: Row(
-          children: [],
+        child: Column(
+          children: [
+            _Title(),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '제목' * 100,
+      maxLines: 1,
+      style: Theme.of(context).textTheme.titleLarge,
+      overflow: TextOverflow.ellipsis,
     );
   }
 }
