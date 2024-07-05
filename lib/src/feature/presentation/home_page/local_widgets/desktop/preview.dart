d1a6b03d3cc2d7385948cb3ab9229641d3b3e64c
KimKyungWon0519 <gwon080870@gmail.com>
2024-07-05 16:27:47 +0900

    FEAT: Add Default Layout on Preview Widget

--- lib/src/feature/presentation/home_page/local_widgets/desktop/preview.dart
+++ lib/src/feature/presentation/home_page/local_widgets/desktop/preview.dart
@@ -5,6 +5,14 @@ class Preview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      margin: const EdgeInsets.all(8),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          children: [],
+        ),
+      ),
+    );
   }
 }
