bfba08751ee7abc3f493c2275a5456a6fd1cf5fa
KimKyungWon0519 <gwon080870@gmail.com>
2024-07-24 02:04:18 +0900

    FEAT: Comment

--- lib/src/feature/presentation/post_page/post_page.dart
+++ lib/src/feature/presentation/post_page/post_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ui_web';
+
 import 'package:flutter/material.dart';
 import 'package:kkw_blog/src/core/utils/models/post.dart';
+import 'package:web/web.dart';
 
 import 'local_widgets/body.dart';
 import 'local_widgets/header.dart';
@@ -14,6 +17,17 @@ class PostPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    platformViewRegistry.registerViewFactory(
+      'comment',
+      (int viewId, {Object? params}) {
+        return HTMLScriptElement()
+          ..src = 'https://utteranc.es/client.js'
+          ..async = true
+          ..style.width = '100%'
+          ..style.height = '100%';
+      },
+    );
+
     return Scaffold(
       body: SelectionArea(
         child: SingleChildScrollView(
@@ -26,6 +40,18 @@ class PostPage extends StatelessWidget {
                 children: [
                   Header(post),
                   Body(post.content),
+                  const SizedBox(
+                    height: 400,
+                    child: HtmlElementView(
+                      viewType: 'comment',
+                      creationParams: {
+                        'repo': "KimKyungWon0519/KimKyungWon0519.github.io",
+                        'issue-term': "pathname",
+                        'theme': "github-light",
+                        'crossorigin': "anonymous",
+                      },
+                    ),
+                  ),
                 ],
               ),
             ),
