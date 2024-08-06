e378b95f0544632fb3810bc7cf7c0bb5a823ed2d
KimKyungWon0519 <gwon080870@gmail.com>
2024-08-06 17:29:37 +0900

    FEAT: Remove unusing package

--- lib/src/pages/home_page/local_widgets/desktop/attribute.dart
+++ lib/src/pages/home_page/local_widgets/desktop/attribute.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kkw_blog/src/constants/app_constant.dart';
