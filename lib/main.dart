b023193ac2acdf1b27582b1c084e2c07e433b45f
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-17 18:08:03 +0900

    FEAT: Root 위젯에 ProviderScope 추가 및 MaterialApp에 route 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kkw_blog/src/core/routes/app_pages.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +11,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: AppPages.routeConfigs,
     );
   }
 }
