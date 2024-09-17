a9a0cfcb93c9c74af2d9016ef364800b5c0d1464
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-17 18:30:45 +0900

    FEAT: BlogTitle 위젯 초기 생성

--- lib/src/presentation/pages/main_page/main_page.dart
+++ lib/src/presentation/pages/main_page/main_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'local_widgets/blog_title.dart';
+
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text('This is MainPage'),
+      body: BlogTitle(),
     );
   }
 }
