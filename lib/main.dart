b5fea4b4b587d8a4004b902eb5f0baa7f39b235c
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-17 18:28:21 +0900

    FEAT: 라이트모드, 다크 모드 테마 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kkw_blog/src/core/routes/app_pages.dart';
+import 'package:kkw_blog/src/core/values/theme.dart';
 
 void main() {
   runApp(const ProviderScope(child: MainApp()));
@@ -12,6 +13,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: lightThemeData,
+      darkTheme: darkThemeData,
       routerConfig: AppPages.routeConfigs,
     );
   }
---
+++ lib/src/core/values/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final ThemeData lightThemeData = ThemeData(
+  scaffoldBackgroundColor: Colors.white,
+  brightness: Brightness.light,
+);
+
+final ThemeData darkThemeData = ThemeData(
+  scaffoldBackgroundColor: Colors.black,
+  brightness: Brightness.dark,
+);
