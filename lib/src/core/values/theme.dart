57f617ca123ab02076436e869cffa7d90a6ff6e2
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-19 15:21:32 +0900

    FEAT: App의 color sheme와 theme 추가

--- lib/src/core/values/theme.dart
+++ lib/src/core/values/theme.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:kkw_blog/src/core/values/color_sheme.dart';
 
 final ThemeData lightThemeData = ThemeData(
   scaffoldBackgroundColor: Colors.white,
+  colorScheme: lightScheme(),
   brightness: Brightness.light,
 );
 
 final ThemeData darkThemeData = ThemeData(
   scaffoldBackgroundColor: Colors.black,
+  colorScheme: darkScheme(),
   brightness: Brightness.dark,
 );
