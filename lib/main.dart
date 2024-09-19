d335afa2d49f7b17360b547cfa1730fba6ee5ef2
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-19 19:36:20 +0900

    FEAT: ThemeMode 초기화 및 FAB 이벤트에 따라 themeMode 변경

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:kkw_blog/resource/l10n/generated/l10n.dart';
+import 'package:kkw_blog/src/core/riverpods/theme_mode_notifier.dart';
 import 'package:kkw_blog/src/core/routes/app_pages.dart';
 import 'package:kkw_blog/resource/values/theme.dart';
 import 'package:responsive_builder/responsive_builder.dart';
@@ -33,23 +34,44 @@ class MainApp extends StatelessWidget {
 
         return ScreenUtilInit(
           designSize: size,
-          builder: (context, child) => MaterialApp.router(
-            localizationsDelegates: const [
-              Messages.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: Messages.delegate.supportedLocales,
-            theme: AppTheme.lightTheme,
-            highContrastTheme: AppTheme.lightHighContrastThemeData,
-            darkTheme: AppTheme.darkTheme,
-            highContrastDarkTheme: AppTheme.darkHighContrastThemeData,
-            themeMode: ThemeMode.dark,
-            routerConfig: AppPages.routeConfigs,
-          ),
+          builder: (context, child) => const _MyMaterialApp(),
         );
       },
     );
+  }
+}
+
+class _MyMaterialApp extends ConsumerWidget {
+  const _MyMaterialApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    _intializeThemeMode(context, ref);
+
+    return MaterialApp.router(
+      localizationsDelegates: const [
+        Messages.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: Messages.delegate.supportedLocales,
+      theme: AppTheme.lightTheme,
+      highContrastTheme: AppTheme.lightHighContrastThemeData,
+      darkTheme: AppTheme.darkTheme,
+      highContrastDarkTheme: AppTheme.darkHighContrastThemeData,
+      themeMode: ref.watch(themeModeNotifierProvider),
+      routerConfig: AppPages.routeConfigs,
+    );
+  }
+
+  void _intializeThemeMode(BuildContext context, WidgetRef ref) {
+    if (ref.read(themeModeNotifierProvider) == ThemeMode.system) {
+      Future(() {
+        Brightness brightness = MediaQuery.of(context).platformBrightness;
+
+        ref.read(themeModeNotifierProvider.notifier).initialize(brightness);
+      });
+    }
   }
 }
--- lib/src/presentation/pages/main_page/local_widgets/theme_mode_fab.dart
+++ lib/src/presentation/pages/main_page/local_widgets/theme_mode_fab.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kkw_blog/src/core/riverpods/theme_mode_notifier.dart';
 
-class ThemeModeFab extends StatelessWidget {
+class ThemeModeFab extends ConsumerWidget {
   const ThemeModeFab({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ThemeMode themeMode = ref.watch(themeModeNotifierProvider);
+
     return FloatingActionButton(
-      onPressed: () {},
-      child: const Icon(Icons.dark_mode),
+      onPressed: () => ref.read(themeModeNotifierProvider.notifier).update(),
+      child: Icon(
+        themeMode == ThemeMode.light ? Icons.dark_mode : Icons.light_mode,
+      ),
     );
   }
 }
