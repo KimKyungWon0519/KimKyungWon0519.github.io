94b5b0d7bc30c14527921700d47aa832226a35e8
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-19 15:22:52 +0900

    FEAT: ProfileCircle 생성

--- lib/src/presentation/pages/main_page/local_widgets/blog_title.dart
+++ lib/src/presentation/pages/main_page/local_widgets/blog_title.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kkw_blog/src/core/constants/assets.dart' as Assets;
 
 class BlogTitle extends StatelessWidget {
   const BlogTitle({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const _ProfileImage();
+  }
+}
+
+class _ProfileImage extends StatelessWidget {
+  const _ProfileImage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.sp,
+      height: 50.sp,
+      decoration: BoxDecoration(
+        image: const DecorationImage(
+          image: AssetImage(Assets.profile),
+        ),
+        border: Border.all(
+          color: Theme.of(context).primaryColor,
+          width: 1,
+        ),
+        shape: BoxShape.circle,
+      ),
+    );
   }
 }
