6df14ffcc22dafee1bbc31928c044dda52a2d2f5
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-19 15:35:14 +0900

    FEAT: GmarketSans font 적용

--- lib/src/core/values/theme.dart
+++ lib/src/core/values/theme.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:kkw_blog/src/core/values/color_sheme.dart';
 
 final ThemeData lightThemeData = ThemeData(
+  fontFamily: 'GmarketSans',
   scaffoldBackgroundColor: Colors.white,
   colorScheme: lightScheme(),
   brightness: Brightness.light,
 );
 
 final ThemeData darkThemeData = ThemeData(
+  fontFamily: 'GmarketSans',
   scaffoldBackgroundColor: Colors.black,
   colorScheme: darkScheme(),
   brightness: Brightness.dark,
