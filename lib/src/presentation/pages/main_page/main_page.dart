f0ac2050b68ea62bef4a808b5380fc77396ccb35
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-19 19:55:11 +0900

    FEAT: 스크롤 위젯 추가

--- lib/src/presentation/pages/main_page/main_page.dart
+++ lib/src/presentation/pages/main_page/main_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:kkw_blog/resource/l10n/generated/l10n.dart';
-import 'package:kkw_blog/src/presentation/pages/main_page/local_widgets/introduction.dart';
-import 'package:kkw_blog/src/presentation/pages/main_page/local_widgets/theme_mode_fab.dart';
 
+import 'local_widgets/introduction.dart';
+import 'local_widgets/theme_mode_fab.dart';
 import 'local_widgets/blog_title.dart';
+import 'local_widgets/classified_panel.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -14,20 +15,23 @@ class MainPage extends StatelessWidget {
       color: Colors.white,
       title: Messages.of(context).blogTitle,
       child: Scaffold(
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: kToolbarHeight,
-              horizontal: 16,
-            ),
-            constraints: const BoxConstraints(maxWidth: 1200),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                BlogTitle(),
-                SizedBox(height: 10),
-                Introduction(),
-              ],
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: kToolbarHeight,
+                horizontal: 16,
+              ),
+              constraints: const BoxConstraints(maxWidth: 1200),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  BlogTitle(),
+                  SizedBox(height: 10),
+                  Introduction(),
+                  ClassifiedPanel(),
+                ],
+              ),
             ),
           ),
         ),
