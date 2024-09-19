5265e65e1e8cfc641394ab806ea0d11f1636f91c
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-19 17:49:12 +0900

    STYLE: 다국어 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:kkw_blog/resource/l10n/generated/l10n.dart';
 import 'package:kkw_blog/src/core/routes/app_pages.dart';
 import 'package:kkw_blog/src/core/values/theme.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
   runApp(const ProviderScope(child: MainApp()));
 }
 
@@ -27,6 +34,13 @@ class MainApp extends StatelessWidget {
         return ScreenUtilInit(
           designSize: size,
           builder: (context, child) => MaterialApp.router(
+            localizationsDelegates: const [
+              Messages.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: Messages.delegate.supportedLocales,
             theme: AppTheme.lightTheme,
             highContrastTheme: AppTheme.lightHighContrastThemeData,
             darkTheme: AppTheme.darkTheme,
