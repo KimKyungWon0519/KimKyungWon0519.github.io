1987fa90855b8ce594e9abe31378e293fe646c0a
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-24 15:28:00 +0900

    FEAT: PostsTable 관련 상수 생성 (이름/컬럼이름)

--- lib/src/core/constants/supabase.dart
+++ lib/src/core/constants/supabase.dart
@@ -1,2 +1,12 @@
 const supabaseUrl = 'https://hzrvlmmdedvyxwnjunyf.supabase.co';
 const supabaseKey = String.fromEnvironment('supabase_key');
+
+final class PostsTable {
+  const PostsTable._();
+
+  static const String name = 'posts';
+
+  static const String fileName = 'file_name';
+  static const String path = 'path';
+  static const String uploadDate = 'upload_date';
+}
--- lib/src/data/data_sources/supabase_database_service.dart
+++ lib/src/data/data_sources/supabase_database_service.dart
@@ -1,9 +1,14 @@
+import 'package:kkw_blog/src/core/constants/supabase.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseDatabaseService {
   final SupabaseClient _client = Supabase.instance.client;
 
   Future<List> getAllPosts({int limit = 50}) {
-    return _client.from('posts').select().limit(limit);
+    return _client
+        .from(PostsTable.name)
+        .select()
+        .limit(limit)
+        .order(PostsTable.uploadDate);
   }
 }
