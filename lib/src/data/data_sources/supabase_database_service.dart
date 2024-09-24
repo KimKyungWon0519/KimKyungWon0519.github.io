6746491b5c3893c691386a75c26fdb2f97c751d2
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-24 17:35:02 +0900

    FEAT: limit 함수를 쓰지 않고 Range 함수 사용

--- lib/src/data/data_sources/supabase_database_service.dart
+++ lib/src/data/data_sources/supabase_database_service.dart
@@ -4,11 +4,11 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseDatabaseService {
   final SupabaseClient _client = Supabase.instance.client;
 
-  Future<List> getAllPosts({int limit = 50}) {
+  Future<List> getAllPosts({int startIndex = 0, int limit = 50}) {
     return _client
         .from(PostsTable.name)
         .select()
-        .limit(limit)
-        .order(PostsTable.uploadDate);
+        .order(PostsTable.uploadDate)
+        .range(startIndex, startIndex + limit - 1);
   }
 }
