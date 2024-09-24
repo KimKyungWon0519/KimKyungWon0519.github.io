2c61a1974e9fddb83b1526ae34c5547a0416aaa2
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-24 14:58:22 +0900

    FEAT: PostDB의 모든 데이터 가져옴

--- lib/src/data/data_sources/supabase_database_service.dart
+++ lib/src/data/data_sources/supabase_database_service.dart
@@ -2,4 +2,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseDatabaseService {
   final SupabaseClient _client = Supabase.instance.client;
+
+  Future<List> getAllPosts({int limit = 50}) {
+    return _client.from('posts').select().limit(limit);
+  }
 }
