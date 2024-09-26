be505fa5e63c2303a89a4804e6ecd4882b6a55d3
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-26 15:41:47 +0900

    FEAT: 마크다운 내 머릿말과 내용 가져오는 기능 추가

--- lib/src/core/utils/markdown_formatter.dart
+++ lib/src/core/utils/markdown_formatter.dart
@@ -1,3 +1,20 @@
+import 'package:kkw_blog/src/core/constants/markdown_constant.dart';
+import 'package:yaml/yaml.dart';
+
 class MarkdownFormatter {
   const MarkdownFormatter._();
+
+  static YamlMap getFrontMatter(String markdown) {
+    int closeIndex = markdown.lastIndexOf(frontMatterDelimiter);
+    String frontMatterRaw =
+        markdown.substring(frontMatterDelimiter.length, closeIndex);
+
+    return loadYaml(frontMatterRaw);
+  }
+
+  static String getContent(String markdown) {
+    int closeIndex = markdown.lastIndexOf(frontMatterDelimiter);
+
+    return markdown.substring(closeIndex + frontMatterDelimiter.length).trim();
+  }
 }
