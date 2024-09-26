8d6e67091f4e846832009fbd4217d290b89096d9
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-26 17:19:37 +0900

    FEAT: ID값을 이용하여  Category 가져옴

--- lib/src/data/data_sources/supabase_database_service.dart
+++ lib/src/data/data_sources/supabase_database_service.dart
@@ -1,4 +1,5 @@
 import 'package:kkw_blog/src/core/constants/supabase.dart';
+import 'package:kkw_blog/src/data/entities/category.dart';
 import 'package:kkw_blog/src/data/entities/post.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -12,5 +13,14 @@ class SupabaseDatabaseService {
         .order(PostsTable.createAt)
         .range(startIndex, startIndex + limit - 1)
         .then((value) => value.map((data) => Post.fromJson(data)).toList());
+  }
+
+  Future<Category> getCategory(int id) {
+    return _client
+        .from(CategoriesTable.table)
+        .select()
+        .eq(CategoriesTable.id, id)
+        .single()
+        .then((value) => Category.fromJson(value));
   }
 }
