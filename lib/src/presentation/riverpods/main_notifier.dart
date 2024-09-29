3bd90a145d70feef95a0f68066cf948ec28e1679
KimKyungWon0519 <gwon080870@gmail.com>
2024-09-29 16:47:45 +0900

    FEAT: MainNotifier에 SupabaseStorageRepo 추가

--- lib/src/presentation/riverpods/main_notifier.dart
+++ lib/src/presentation/riverpods/main_notifier.dart
@@ -1,12 +1,20 @@
+import 'package:kkw_blog/src/dependency_injection.dart';
 import 'package:kkw_blog/src/domain/models/classification_type.dart';
+import 'package:kkw_blog/src/domain/repositories/supabase_stroage_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'main_notifier.g.dart';
 
 @riverpod
 class MainNotifier extends _$MainNotifier {
+  late final SupabaseStorageRepository _supabaseStorageRepository;
+
   @override
-  ClassificationType build() => const AllType(0);
+  ClassificationType build() {
+    _supabaseStorageRepository = instance<SupabaseStorageRepository>();
+
+    return const AllType(0);
+  }
 
   void update(ClassificationType type) => state = type;
 }
