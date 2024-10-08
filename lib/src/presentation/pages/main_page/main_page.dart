ef51a81a73b08d022e690fa87cab3ebff22e81af
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-08 19:31:48 +0900

    FEAT: 스크롤이 하단 근처 일 때 posts 업데이트

--- lib/src/presentation/pages/main_page/main_page.dart
+++ lib/src/presentation/pages/main_page/main_page.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kkw_blog/resource/l10n/generated/l10n.dart';
+import 'package:kkw_blog/src/presentation/riverpods/main_notifier.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 import 'local_widgets/desktop_view/layout.dart' as Desktop;
 import 'local_widgets/mobile_view/layout.dart' as Mobile;
 import 'local_widgets/theme_mode_fab.dart';
 
-class MainPage extends HookWidget {
+class MainPage extends HookConsumerWidget {
   const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool isUpdate = false;
     final ScrollController scrollController = useScrollController();
+
+    scrollController.addListener(() {
+      if (!isUpdate &&
+          scrollController.offset >=
+              scrollController.position.maxScrollExtent * 0.8) {
+        isUpdate = true;
+
+        ref
+            .read(mainNotifierProvider.notifier)
+            .increaseStartOffset()
+            .then((value) => isUpdate = false);
+      }
+    });
 
     return Title(
       color: Colors.white,
--- lib/src/presentation/riverpods/main_notifier.dart
+++ lib/src/presentation/riverpods/main_notifier.dart
@@ -3,6 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:kkw_blog/src/dependency_injection.dart';
 import 'package:kkw_blog/src/domain/models/classification_type.dart';
 import 'package:kkw_blog/src/domain/models/post.dart';
+import 'package:kkw_blog/src/domain/repositories/supabase_database_repository.dart';
 import 'package:kkw_blog/src/domain/repositories/supabase_stroage_repository.dart';
 import 'package:kkw_blog/src/presentation/riverpods/classified_types_notifier.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -13,6 +14,11 @@ part 'main_notifier.g.dart';
 @riverpod
 class MainNotifier extends _$MainNotifier {
   late final SupabaseStorageRepository _supabaseStorageRepository;
+
+  int _maxLength = 0;
+  int _startOffset = 0;
+  int? _categoryID;
+  int? _tagID;
 
   MainNotifier()
       : _supabaseStorageRepository = instance<SupabaseStorageRepository>();
@@ -28,46 +34,61 @@ class MainNotifier extends _$MainNotifier {
     state = state.copyWith(type: type);
   }
 
+  Future<void> increaseStartOffset() async {
+    if (_startOffset > _maxLength) return;
+
+    _startOffset += 50;
+
+    List<Post> posts = await _updatePosts();
+
+    insertPost(posts);
+  }
+
   set posts(List<Post> posts) => state = state.copyWith(posts: posts);
+  void insertPost(List<Post> posts) {
+    this.posts = state.posts.toList()..addAll(posts);
+  }
 
-  void updatePostsWithType(ClassificationType type) {
-    switch (type) {
-      case AllType():
-        _updatePosts();
+  void updatePostsWithType(ClassificationType type) async {
+    _startOffset = 0;
+    _categoryID = null;
+    _tagID = null;
 
-        break;
-      case CategoryType():
-        _updatePosts(categoryID: type.id);
-
-        break;
-      case TagType():
-        _updatePosts(tagID: type.id);
-
-        break;
+    if (type case CategoryType()) {
+      _categoryID = type.id;
+    } else if (type case TagType()) {
+      _tagID = type.id;
     }
+
+    List<Post> posts = await _updatePosts();
+
+    this.posts = posts;
   }
 
   void _initalizeState() {
-    _updatePosts();
+    _initalizePosts();
     _initalizeType();
   }
 
-  Future<void> _updatePosts({
-    int? categoryID,
-    int? tagID,
-  }) async {
-    List<Post> posts = await _supabaseStorageRepository.getPostFiles(
-      tagID: tagID,
-      categoryID: categoryID,
-    );
+  void _initalizePosts() {
+    _updatePosts().then((value) => posts = value);
+  }
 
-    this.posts = posts;
+  Future<List<Post>> _updatePosts() async {
+    return _supabaseStorageRepository.getPostFiles(
+      startOffset: _startOffset,
+      tagID: _tagID,
+      categoryID: _categoryID,
+    );
   }
 
   void _initalizeType() {
     AsyncValue<AllType> type = ref.watch(allTypeNotifierProvider);
 
-    type.whenData((data) => this.type = data);
+    type.whenData((data) {
+      this.type = data;
+      _maxLength = data.count;
+    });
   }
 }
 
