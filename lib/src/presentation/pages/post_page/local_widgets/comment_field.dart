80eb7495ce248e3c5017f02d666a6d47adff418c
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-13 16:52:20 +0900

    FEAT: InputField 생성

--- lib/src/presentation/pages/post_page/local_widgets/comment_field.dart
+++ lib/src/presentation/pages/post_page/local_widgets/comment_field.dart
@@ -8,14 +8,40 @@ class CommentField extends StatelessWidget {
     double height = MediaQuery.sizeOf(context).height;
 
     return Container(
-      constraints: BoxConstraints(
-        minHeight: height * 0.2,
-        maxHeight: height * 0.6,
-      ),
+      height: height * 0.25,
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.onPrimaryContainer,
         borderRadius: BorderRadius.circular(5),
+      ),
+      child: const _InputField(),
+    );
+  }
+}
+
+class _InputField extends StatelessWidget {
+  const _InputField();
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).colorScheme.primaryContainer;
+
+    return TextField(
+      expands: true,
+      maxLines: null,
+      maxLength: 500,
+      cursorColor: color,
+      style: TextStyle(color: color),
+      textAlignVertical: TextAlignVertical.top,
+      decoration: InputDecoration(
+        counterStyle: TextStyle(color: color),
+        border: const OutlineInputBorder(),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: color),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: color),
+        ),
       ),
     );
   }
