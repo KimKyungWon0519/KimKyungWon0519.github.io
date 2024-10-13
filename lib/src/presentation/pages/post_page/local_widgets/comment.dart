82a134d8b79adb20324ea7bfa0e612922ac5bca3
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-13 16:18:55 +0900

    FEAT: 댓글 기본 레이아웃 추가

--- lib/src/presentation/pages/post_page/local_widgets/comment.dart
+++ lib/src/presentation/pages/post_page/local_widgets/comment.dart
@@ -5,6 +5,18 @@ class Comment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double height = MediaQuery.sizeOf(context).height;
+
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: height * 0.2,
+        maxHeight: height * 0.6,
+      ),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.onPrimaryContainer,
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
   }
 }
