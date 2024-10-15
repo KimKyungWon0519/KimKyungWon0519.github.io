0865afecd0232d0a863ecdaa3b707c687ea3c5e7
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-15 18:58:31 +0900

    FEAT: 댓글 사이즈 고정

--- lib/src/presentation/pages/post_page/local_widgets/comment_field.dart
+++ lib/src/presentation/pages/post_page/local_widgets/comment_field.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kkw_blog/resource/l10n/generated/l10n.dart';
 import 'package:kkw_blog/resource/values/theme.dart';
 
@@ -16,10 +17,8 @@ class CommentField extends HookWidget {
   Widget build(BuildContext context) {
     final TabController tabController = useTabController(initialLength: 2);
 
-    final double height = MediaQuery.sizeOf(context).height;
-
     return Container(
-      height: height * 0.35,
+      height: 350,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
       ),
