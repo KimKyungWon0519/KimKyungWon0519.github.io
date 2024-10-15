dbcc13ac1a44585efb2dab1effde99cc46296078
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-15 15:26:02 +0900

    FEAT: 텍스트 변경 및 Tab 추가

--- lib/src/presentation/pages/post_page/local_widgets/comment_field.dart
+++ lib/src/presentation/pages/post_page/local_widgets/comment_field.dart
@@ -1,20 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:kkw_blog/resource/l10n/generated/l10n.dart';
 
-class CommentField extends StatelessWidget {
+class CommentField extends HookWidget {
   const CommentField({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.sizeOf(context).height;
+    final TabController tabController = useTabController(initialLength: 2);
+    final double height = MediaQuery.sizeOf(context).height;
 
     return Container(
-      height: height * 0.25,
-      padding: const EdgeInsets.all(16),
+      height: height * 0.3,
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.onPrimaryContainer,
         borderRadius: BorderRadius.circular(5),
       ),
-      child: const _InputField(),
+      clipBehavior: Clip.hardEdge,
+      child: Column(
+        children: [
+          _Header(controller: tabController),
+          _Body(controller: tabController),
+        ],
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  final TabController controller;
+
+  const _Header({
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      controller: controller,
+      dividerColor: Colors.transparent,
+      unselectedLabelColor: Colors.grey,
+      physics: const NeverScrollableScrollPhysics(),
+      isScrollable: true,
+      tabAlignment: TabAlignment.start,
+      indicatorSize: TabBarIndicatorSize.tab,
+      overlayColor: WidgetStateProperty.all(Colors.transparent),
+      indicator: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(5),
+          topRight: Radius.circular(5),
+        ),
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      labelColor: Theme.of(context).colorScheme.primaryContainer,
+      tabs: [
+        Tab(text: Messages.of(context).write),
+        Tab(text: Messages.of(context).preview),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final TabController controller;
+
+  const _Body({
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Theme.of(context).colorScheme.primary,
+        padding: const EdgeInsets.all(8),
+        child: TabBarView(
+          controller: controller,
+          children: const [
+            _InputField(),
+            _Preview(),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -26,23 +94,35 @@ class _InputField extends StatelessWidget {
   Widget build(BuildContext context) {
     Color color = Theme.of(context).colorScheme.primaryContainer;
 
-    return TextField(
-      expands: true,
-      maxLines: null,
-      maxLength: 500,
-      cursorColor: color,
-      style: TextStyle(color: color),
-      textAlignVertical: TextAlignVertical.top,
-      decoration: InputDecoration(
-        counterStyle: TextStyle(color: color),
-        border: const OutlineInputBorder(),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: color),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: color),
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: TextField(
+        expands: true,
+        maxLines: null,
+        maxLength: 500,
+        cursorColor: color,
+        style: TextStyle(color: color),
+        textAlignVertical: TextAlignVertical.top,
+        decoration: InputDecoration(
+          counterStyle: TextStyle(color: color),
+          border: const OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: color),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: color),
+          ),
         ),
       ),
     );
+  }
+}
+
+class _Preview extends StatelessWidget {
+  const _Preview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Markdown(data: 'data');
   }
 }
--- lib/src/presentation/widgets/scroll_up_fab.dart
+++ lib/src/presentation/widgets/scroll_up_fab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kkw_blog/resource/l10n/generated/l10n.dart';
 
 class ScrollUpFab extends StatelessWidget {
   final VoidCallback? onPressed;
@@ -13,7 +14,7 @@ class ScrollUpFab extends StatelessWidget {
     return FloatingActionButton.extended(
       heroTag: 'ScrollUpFab',
       onPressed: onPressed,
-      label: const Text('맨 위로'),
+      label: Text(Messages.of(context).scrollUp),
       icon: const Icon(Icons.arrow_upward_rounded),
     );
   }
