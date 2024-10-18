0b78c395478ecc802358f192232059c83df1feae
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-18 17:32:32 +0900

    FEAT: SupabaseAuthRepository 생성

---
+++ lib/src/data/repositories/supabase_auth_repository_impl.dart
@@ -0,0 +1,6 @@
+import 'package:kkw_blog/src/data/data_sources/supabase_auth_service.dart';
+import 'package:kkw_blog/src/domain/repositories/supabase_auth_repository.dart';
+
+class SupabaseAuthRepesitoryImpl implements SupabaseAuthRepository {
+  late final SupabaseAuthService _authService;
+}
