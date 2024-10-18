4eda6d43d534e6313f8481fb52d17c65a2c84962
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-18 17:30:16 +0900

    FEAT: SupabaseAuthSerivce  이름변경

--- lib/src/data/data_sources/supabase_auth_service.dart
+++ lib/src/data/data_sources/supabase_auth_service.dart
@@ -1,6 +1,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class SupbaseAuthService {
+class SupabaseAuthService {
   final GoTrueClient auth = Supabase.instance.client.auth;
 
   Future<bool> loginWithGithub() async {
