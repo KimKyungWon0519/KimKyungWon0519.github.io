8049722ed343eeab7cb850059a8e6a9c738fb0a1
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-18 17:43:33 +0900

    FEAT: PostNotifier 내에 SupabaseAuthRepository 인스턴스 생성

--- lib/src/presentation/riverpods/post_notifier.dart
+++ lib/src/presentation/riverpods/post_notifier.dart
@@ -1,5 +1,6 @@
 import 'package:kkw_blog/src/dependency_injection.dart';
 import 'package:kkw_blog/src/domain/models/post.dart';
+import 'package:kkw_blog/src/domain/repositories/supabase_auth_repository.dart';
 import 'package:kkw_blog/src/domain/repositories/supabase_stroage_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -8,9 +9,11 @@ part 'post_notifier.g.dart';
 @riverpod
 class PostNotifier extends _$PostNotifier {
   late final SupabaseStorageRepository _supabaseStorageRepository;
+  late final SupabaseAuthRepository _supabaseAuthRepository;
 
   PostNotifier()
-      : _supabaseStorageRepository = instance<SupabaseStorageRepository>();
+      : _supabaseStorageRepository = instance<SupabaseStorageRepository>(),
+        _supabaseAuthRepository = instance<SupabaseAuthRepository>();
 
   @override
   Post? build() => null;
