06be0cbe2a9b9d3499d55b1f5b86428a0c5d697e
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-18 16:34:00 +0900

    STYLE: Github Icon 추가

--- lib/resource/assets.dart
+++ lib/resource/assets.dart
@@ -1,2 +1,5 @@
 const String _images = 'assets/images';
+const String _icons = 'assets/icons';
+
 const String profile = '$_images/profile.jpeg';
+const String githubLogo = '$_icons/github-mark.svg';
