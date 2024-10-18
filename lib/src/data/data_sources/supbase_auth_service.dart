47cf6969e625fd79d13289a93a5e011974be9467
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-18 17:18:31 +0900

    FEAT: 깃허브 로그인 추가

--- lib/src/data/data_sources/supbase_auth_service.dart
+++ lib/src/data/data_sources/supbase_auth_service.dart
@@ -2,4 +2,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupbaseAuthService {
   final GoTrueClient auth = Supabase.instance.client.auth;
+
+  Future<bool> loginWithGithub() async {
+    return auth.signInWithOAuth(OAuthProvider.github);
+  }
 }
