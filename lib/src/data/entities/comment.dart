85455f37c350ca06d666e7c2397520ea287acf22
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-23 18:02:54 +0900

    FEAT: Comment Entity생성

---
+++ lib/src/data/entities/comment.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kkw_blog/src/core/constants/supabase.dart';
+
+part 'comment.freezed.dart';
+part 'comment.g.dart';
+
+@freezed
+class Comment with _$Comment {
+  const factory Comment({
+    required int? id,
+    required String content,
+    required String userUUID,
+    @JsonKey(name: CommentsTable.createAt) required DateTime createAt,
+  }) = _Comment;
+
+  factory Comment.fromJson(Map<String, dynamic> json) =>
+      _$CommentFromJson(json);
+}
