a53f23536cb32fe43f2e265f0fdc834b113f8f6a
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-31 19:40:05 +0900

    FEAT: Model.Favorite 생성 및 Entity -> Model Mapper 생성

---
+++ lib/src/data/mappers/favorite_mapper.dart
@@ -0,0 +1,12 @@
+import 'package:kkw_blog/src/data/entities/favorite.dart' as Entity;
+import 'package:kkw_blog/src/domain/models/favorite.dart' as Model;
+
+extension FavoriteEntityMapper on Entity.Favorite {
+  Model.Favorite toModel() {
+    return Model.Favorite(
+      id: id,
+      uuid: uuid,
+      postID: postID,
+    );
+  }
+}
---
+++ lib/src/domain/models/favorite.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'favorite.freezed.dart';
+
+@freezed
+class Favorite with _$Favorite {
+  const factory Favorite({
+    required int id,
+    required String uuid,
+    required int postID,
+  }) = _Favorite;
+}
