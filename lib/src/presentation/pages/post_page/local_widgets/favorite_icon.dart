c6f3d0752ac68cc525589b3f3379a844df009007
KimKyungWon0519 <gwon080870@gmail.com>
2024-10-31 20:06:03 +0900

    FEAT: PostPage/FavoriteIcon에서 Favorites 상태 추가

--- lib/src/presentation/pages/post_page/local_widgets/favorite_icon.dart
+++ lib/src/presentation/pages/post_page/local_widgets/favorite_icon.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kkw_blog/src/domain/models/favorite.dart';
+import 'package:kkw_blog/src/presentation/riverpods/post_notifier.dart';
 
-class FavoriteIcon extends StatelessWidget {
+class FavoriteIcon extends ConsumerWidget {
   const FavoriteIcon({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<Favorite> favorite =
+        ref.watch(postNotifierProvider.select((value) => value.favorites));
+
     return ElevatedButton.icon(
       style: ButtonStyle(
         backgroundColor: WidgetStatePropertyAll(
@@ -13,7 +19,7 @@ class FavoriteIcon extends StatelessWidget {
       ),
       onPressed: () {},
       icon: const Icon(Icons.favorite_border_rounded),
-      label: Text('1'),
+      label: Text('${favorite.length}'),
     );
   }
 }
