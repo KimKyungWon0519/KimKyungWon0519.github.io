54db8591f21894c947177e24d8fa9f138cd12219
KimKyungWon0519 <gwon080870@gmail.com>
2025-02-14 21:02:45 +0900

    FEAT: 마크다운 코드 스타일 설정

--- lib/src/presentation/pages/post_page/local_widgets/markdown_view.dart
+++ lib/src/presentation/pages/post_page/local_widgets/markdown_view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_highlighter/flutter_highlighter.dart';
+import 'package:flutter_highlighter/themes/atom-one-dark.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:kkw_blog/resource/values/theme.dart';
 import 'package:kkw_blog/src/core/constants/supabase.dart';
+import 'package:markdown/markdown.dart' as md;
 
 class MarkdownView extends StatelessWidget {
   final String routeID;
@@ -21,6 +25,13 @@ class MarkdownView extends StatelessWidget {
       softLineBreak: true,
       styleSheet: customMarkdownStyleSheet(context),
       data: content,
+      inlineSyntaxes: [
+        InlineCodeSyntax(),
+      ],
+      builders: {
+        'inline code': InlineCodeBuilder(),
+        'code': CodeBlockBuilder(),
+      },
       imageBuilder: (uri, title, alt) {
         String originUri = uri.path;
         Widget child;
@@ -37,6 +48,54 @@ class MarkdownView extends StatelessWidget {
           child: child,
         );
       },
+    );
+  }
+}
+
+class InlineCodeSyntax extends md.InlineSyntax {
+  InlineCodeSyntax() : super(r'`(.*?)`');
+
+  @override
+  bool onMatch(md.InlineParser parser, Match match) {
+    final element = md.Element.text("inline code", match[1]!);
+    parser.addNode(element);
+
+    return true;
+  }
+}
+
+class InlineCodeBuilder extends MarkdownElementBuilder {
+  @override
+  Widget visitElementAfter(md.Element element, TextStyle? preferredStyle) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 6),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Text(
+        element.textContent,
+        style: GoogleFonts.robotoMono(color: Colors.white),
+      ),
+    );
+  }
+}
+
+class CodeBlockBuilder extends MarkdownElementBuilder {
+  @override
+  Widget visitElementAfter(md.Element element, TextStyle? preferredStyle) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: HighlightView(
+        element.textContent.substring(0, element.textContent.length - 1),
+        theme: atomOneDarkTheme,
+        language: 'kotlin',
+        padding: const EdgeInsets.all(16),
+        textStyle: GoogleFonts.robotoMono(),
+      ),
     );
   }
 }
